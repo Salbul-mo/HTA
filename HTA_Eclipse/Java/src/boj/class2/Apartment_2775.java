ab101f7a6de23d9661e69dadbaf788ce504c8584
salbul <salbul@TABLET-9PK2QF7E>
2024-10-27 21:16:40 +0900

    dd

--- HTA_Eclipse/Java/src/boj/class2/Apartment_2775.java
+++ HTA_Eclipse/Java/src/boj/class2/Apartment_2775.java
@@ -4,40 +4,41 @@ import java.io.*;
 import java.util.*;
 
 public class Apartment_2775 {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int t = Integer.parseInt(br.readLine()); // 입력 케이스 수
-		int k = Integer.parseInt(br.readLine()); // 입력 층
-		int n = Integer.parseInt(br.readLine()); // 입력 호
-		
-		int[] zero = new int[n];
-		
-		for (int i = 1; i < zero.length; i++) {
-			zero[i] = i;
-		}
-		
-		int[] beforefloor = zero;
-		int[] presentfloor = new int[n];
-		
-		
-		for(int cnt = 2; cnt < k; cnt++) {
-			
-			for (int ho = 0; ho < presentfloor.length; ho++) {
-				
-				int sum = 0;
-				
-				for (int before = 0; before <= ho; before++) {
-					sum += beforefloor[before];
-				}
-				presentfloor[ho] = sum;
-			}
-		}
-		
-		
-	}
 
+		int t = Integer.parseInt(br.readLine()); // 입력 케이스 수
+		
+		
+		for (int c = 0; c < t; c++) {
+			int k = Integer.parseInt(br.readLine()); // 입력 층
+			int n = Integer.parseInt(br.readLine()); // 입력 호
+
+			
+			int[] present = new int[n];
+
+			for (int j = 1; j <= k; j++) { // 1층 부터 시작
+				
+				for (int ho = 1; ho <= n; ho++) {
+					present[ho-1] = 0;
+					
+					for (int bef = 1; bef <= ho; bef++) {
+						present[ho-1] += before[bef-1];
+					}
+				}
+				
+				before = present;
+			
+			}
+			
+			
+			bw.write(Integer.toString(before[n-1])+"\n");
+		
+		}
+		
+		bw.flush();
+	}
 }
